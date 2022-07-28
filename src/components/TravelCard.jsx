3c47fcdeebf44121f4296e2ff650a9de1cc3867d
Mark Linn <marktlinn@gmail.com>
2022-07-28 11:19:44 +0200

    completed layout, made responsive with media queries

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -9,8 +9,10 @@ export default function TravelCard(props){
                     <img className="location-photo" src={img} alt="" />
                 </div>
                 <div className="text-section">
-                    <span className="country map-link-text"><i class="fa-solid fa-location-dot"></i> {location}</span>
-                    <a href={gmaps} className="map-link map-link-text">View on Google Maps</a>
+                    <div className="map-link-text">
+                        <span className="country"><i class="fa-solid fa-location-dot"></i> {location}</span>
+                        <a href={gmaps} className="map-link">View on Google Maps</a>
+                    </div>
                     <h2 className="article-title-text">{title}</h2>
                     <h4 className="date-text">{date}</h4>
                     <p className="article-text">{description}</p>
